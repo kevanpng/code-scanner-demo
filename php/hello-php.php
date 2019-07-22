0aeaf6c2eaa989bfd441af4afba4128631986994
Kevan Png <kevanpngyeowwei@gmail.com>
2019-07-22 14:14:24 +0800

    Update hello-php.php

--- php/hello-php.php
+++ php/hello-php.php
@@ -7,18 +7,4 @@ $uname = "jeff";
 $password = "hunter2";
 connect($uname, $password);
 
-// trigger RSPEC-2068
-$uname = "jeff2";
-$password = "hunter3";
-connect($uname, $password);
-
-// trigger RSPEC-2068
-$uname = "jeff3";
-$password = "hunter4";
-connect($uname, $password);
-
-
-// trigger RSPEC-2068
-$uname = "kev1";
-$password = "kev2";
-connect($uname, $password);
+//removed 3
