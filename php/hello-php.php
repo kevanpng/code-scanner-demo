1e44120fc025e021dcaaea78ea11a3dd1b99e279
Kevan Png <kevanpngyeowwei@gmail.com>
2019-08-01 18:38:27 +0800

    Update hello-php.php

--- php/hello-php.php
+++ php/hello-php.php
@@ -19,9 +19,27 @@ connect($uname, $password);
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // trigger RSPEC-2068
 $uname = "jeff";
 $password = "hunter2";
 connect($uname, $password);
 
-//shift line. params should be different, resulting in 1 new and 1 resolved finding
+//shift line. params should be different, resulting in 1 new and 1 resolved finding. shifted even more
